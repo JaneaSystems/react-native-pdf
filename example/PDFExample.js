71f60104bd8ef405fe9a282eefb59fafb7821d23
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-12-15 19:15:04 +0100

    Use bundled PDF

--- example/PDFExample.js
+++ example/PDFExample.js
@@ -82,10 +82,10 @@ export default class PDFExample extends React.Component {
     };
 
     render() {        
-        let source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
+        //let source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
         //let source = Platform.OS === "windows" ? {uri: 'ms-appx:///test.pdf'} : {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
         //let source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
-        //let source = {uri: 'ms-appx:///test.pdf'}
+        let source = {uri: 'ms-appx:///test.pdf'}
         //let source = require('./test.pdf');  // ios only
         //let source = {uri:'bundle-assets://test.pdf'};
 
